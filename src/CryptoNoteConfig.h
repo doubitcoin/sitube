40691557d633f4af7898741572edb34cf2a76ec7
Gosdor-xda <33892273+gosdor-xda@users.noreply.github.com>
2018-10-12 02:40:45 +0700

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -128,7 +128,7 @@ const uint32_t P2P_IDLE_CONNECTION_KILL_INTERVAL             = (5 * 60);      //
 const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "";
 
 const char* const SEED_NODES[] = { 
-  "209.97.136.116:19044",
+  "159.89.11.247:19044",
 };
 
 struct CheckpointData {
